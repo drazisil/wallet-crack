abf574b9c4ce480ea8e0eb8e6f3fbc7363d83b33
BuildTools <unconfigured@null.spigotmc.org>
2017-07-15 13:10:00 -0400

    Added readme and better resume

---
+++ README.md
@@ -0,0 +1,5 @@
+## wallet-crack
+
+CPU usage: https://www.npmjs.com/package/windows-cpu
+
+Modified code from: https://www.npmjs.com/package/node-bruteforce
--- crack.js
+++ crack.js
@@ -1,10 +1,11 @@
 const spawn = require('child_process').spawn;
+var cpu = require('windows-cpu')
 
 // Path to the citcoin-cli executable
 const BITCOIN_CLI_PATH = 'C:\\Program Files\\Bitcoin\\daemon\\bitcoin-cli.exe'
 
 // How many seconds between tries
-const DELAY = 0.75
+const DELAY = 0.25
 
 function testPassphrase(phraseToTest) {
 	
@@ -35,24 +36,29 @@ function intToCharacterBasedString(characters, num) { // Anoying algorithm..
 		modulo = num % characters.length // Basic calculating
 		charBasedString = characters[modulo] + charBasedString; // Just push it before the old characters
 		num = ((num - modulo) / characters.length); // New value of num, annoying calculation
-	} 
+	}
+
 	return charBasedString;
 };
 
 
-function loop(characters, i, callback) {
-	result = callback( intToCharacterBasedString(characters, i ) );
+
+function loop(startingIndex, characters, i, callback) {
+	
+	// This failsafe will end if the current index can be divided by 1000
+	if (i > 100 && i % 400 === 0 && i !== startingIndex) {
+		console.log(`Current index: ${i}, please try again`)
+		process.exit()
+	}
+	
+	result = callback( intToCharacterBasedString(characters, i ), i );
 		
 	if( result ){ // If callbacks returns true: we did our job!
 		return
 	} else {
-		if (i % 1000 === 0) {
-			console.log(`Current index: ${i}, please try again`)
-			process.exit()
-		}
 		i++;
 		setTimeout(function() {
-			loop(characters, i, callback)
+			loop(startingIndex, characters, i, callback)
 		}, (DELAY * 1000));
 	}
 }
@@ -69,7 +75,7 @@ function bruteForce(counter, characters, callback) {
 	
 	characters = [""].concat(characters); // Useless empty value to start this array on index = 1
 	
-	loop(characters, counter, callback);	
+	loop(counter, characters, counter, callback);	
 }
 
 // ===================================================
@@ -81,15 +87,29 @@ const CHARACTERS_TO_TEST = '!#$%&*0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ^_abcdefg
 const STARTING_INDEX = process.argv[2] || 1
 
 console.log(`Starting index: ${STARTING_INDEX}`)
- 
- bruteForce(STARTING_INDEX, CHARACTERS_TO_TEST, function(value){    
+
+bruteForce(STARTING_INDEX, CHARACTERS_TO_TEST, function(value, index){    
  
 	console.log(`Testing value: ${value}`)
+	
+	// Get load for current running node process
+	cpu.totalLoad(function(error, results) {
+		if(error) {
+			return console.log(error);
+		}
+		
+	console.log(`Total Process Load: ${results[0]}`);
+	
+	// This failsafe will kick in if cpu usage goes over the limit
+	if (results[0] > 60) {
+		console.log(`Cpu usage percent exceeded: ${results[0]}, Current index: ${index}, please try again`)
+		process.exit()
+	}
+		testPassphrase(value)
     
-	testPassphrase(value)
-    
-    return false;
-});
+		return false;
+	});
+ })
 
 
 
