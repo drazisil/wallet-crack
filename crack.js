0fcfd7892cb54383defd1eaff837117565a73dc7
BuildTools <unconfigured@null.spigotmc.org>
2017-07-15 07:23:54 -0400

    better loop

--- crack.js
+++ crack.js
@@ -27,65 +27,60 @@ function testPassphrase(phraseToTest) {
 	});
 }
 
+function intToCharacterBasedString(characters, num) { // Anoying algorithm..
+	var charBasedString, modulo;
+		
+	charBasedString = "";
+
+	while (num > 0) {
+		modulo = num % characters.length // Basic calculating
+		charBasedString = characters[modulo] + charBasedString; // Just push it before the old characters
+		num = ((num - modulo) / characters.length); // New value of num, annoying calculation
+	} 
+
+	console.log(`Testing num: ${num}, value: ${charBasedString}`)
+	return charBasedString;
+};
+
+
+function loop(characters, i, callback) {
+	result = callback( intToCharacterBasedString(characters, i ) );
+		
+	if( result ){ // If callbacks returns true: we did our job!
+		return
+	} else {
+		i++;
+		setTimeout(function() {
+			loop(characters, i, callback)
+		}, (DELAY * 1000));
+	}
+}
+
+
 // https://www.npmjs.com/package/node-bruteforce
 function bruteForce(characters, callback) {
 	
-	var i, intToCharacterBasedString, result, sortedCharacters;
+	var intToCharacterBasedString, result;
 	
-	if(typeof characters == 'string')
+	if(typeof characters == 'string') {
 		characters = characters.split("");
-	
-	// Sort all characters
-	characters.sort();
-	characters = characters.filter(function(value, index, arr){
-		if(index < 1) {
-			return true;
-		} else {
-			return value != arr[index-1];
-		}
-	});
+	}
 	
 	characters = [""].concat(characters); // Useless empty value to start this array on index = 1
 	
-	intToCharacterBasedString = function(num) { // Anoying algorithm..
-		var charBasedString, modulo;
-		
-		charBasedString = "";
-
-		while (num > 0) {
-			modulo = num % characters.length // Basic calculating
-			charBasedString = characters[modulo] + charBasedString; // Just push it before the old characters
-			num = ((num - modulo) / characters.length); // New value of num, annoying calculation
-		} 
-
-		return charBasedString;
-	};
+	let counter = 1;
 	
-	i = 1;
-	
-	(function loop() {
-		result = callback( intToCharacterBasedString( i ) );
-		
-		if( result ){ // If callbacks returns true: we did our job!
-			return
-		} else {
-			i++;
-			setTimeout(function() {
-				loop()
-			}, (DELAY * 1000));
-		}
-	}());	
+	loop(characters, counter, callback);	
 }
 
 
 var hash = 'HELLO';
  
-bruteForce(['!', '#', '$', '%', '&', '*', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', 'A', 
-	'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
-	'V', 'W', 'X', 'Y', 'Z', '^', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], function(value){
+const CHARACTERS_TO_TEST = '!#$%&*0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ^_abcdefghijklmnopqrstuvwxyz'
+ 
+ bruteForce(CHARACTERS_TO_TEST, function(value){
     
-    console.log(`Testing value: ${value}`)
+    
 	testPassphrase(value)
 //	if( hash == value ) {
 //        console.log("Correct value of the hash was: " + value);
